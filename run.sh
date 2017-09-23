256900d5557fb900bb8139502e344b00ca05efe8
kmikami <kmikami@indeed.com>
2017-09-23 17:29:18 -0500

    #20: enable run.sh to use LOG_DIR/OUTPUT_DIR env vars

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 set -eu
-mkdir -p ./log/ ./output/
+log_dir=${LOG_DIR:-./log/}
+output_dir=${OUTPUT_DIR:-./output/}
+mkdir -p $log_dir $output_dir
 program=$1
 input=$2
 case_num=`basename $2 ".txt"`
 datetime=`date +"%Y-%m-%d-%H-%M-%S"`
-filename=$datetime-$case_num
-$program $input 1> ./output/$filename.txt 2> ./log/$filename.log
+filename=$case_num-$datetime
+$program $input 1> $output_dir/$filename.txt 2> $log_dir/$filename.log
