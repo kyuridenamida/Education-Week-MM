381e793ad0170c665ebcea46c6e9db86d61b52ba
kmikami <kmikami@indeed.com>
2017-09-23 02:44:28 -0500

    #15: implement input_generator

---
+++ input_generator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cassert>
+#include <sstream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main(){
+	int N,K;
+	cin >> N >> K;
+	cerr << N << endl;
+	cerr << K << endl;
+	for(int i = 0 ; i < K ; i++){
+		int vi,vj;
+		cin >> vi >> vj;
+		cerr << vi << " " << vj << endl;
+	}
+}
