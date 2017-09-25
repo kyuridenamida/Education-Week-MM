e0cf736e6660b0936c4cef64e26451d473af1dcb
kmikami <kmikami@indeed.com>
2017-09-25 03:23:05 -0500

    NOBUG: minor potential bug fix for build_result_script

--- build_result_from_log.sh
+++ build_result_from_log.sh
@@ -5,8 +5,8 @@ do
 	final_score=`cat $log_dir/$file | grep final_score | sed -e "s/^.* \([0-9]\+\)/\1/g"`
 	n=`cat $log_dir/$file | grep " N " | sed -e "s/^.* \([0-9]\+\)/\1/g"`
 	k=`cat $log_dir/$file | grep " K " | sed -e "s/^.* \([0-9]\+\)/\1/g"`
-	final_t=`cat $log_dir/$file | grep "final_t" | sed -e "s/^.* \([0-9]\+\)/\1/g"`
-	last_updated=`cat $log_dir/$file | grep "last_updated " | sed -e "s/^.* \([0-9]\+\)/\1/g"`
+	final_t=`cat $log_dir/$file | grep " final_t " | sed -e "s/^.* \([0-9]\+\)/\1/g"`
+	last_updated=`cat $log_dir/$file | grep " last_updated " | sed -e "s/^.* \([0-9]\+\)/\1/g"`
 	last_updated_by_probability=`cat $log_dir/$file | grep "last_updated_by_probability " | sed -e "s/^.* \([0-9]\+\)/\1/g"`
 	
 	echo $final_score, $n, $k, $final_t, $last_updated, $last_updated_by_probability
